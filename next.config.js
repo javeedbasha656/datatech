7a8731cc9535ac7ecc0114cf5c9a58192a6e9795
Javeed Basha <37532800+javeedbasha656@users.noreply.github.com>
2022-10-21 08:12:19 -0400

    Add files via upload
    Uploaded the initial files for datatech

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+module.exports = nextConfig
+
+// DremioPersonalToken - wyCNVnwmRzKV5w1fv/D5vLQVqatkDisnEK8MdxJcfC70PAe12XwpKoPNrOfeNg==
+
+
+
+
