dd6e29395fbcd66df5a5c95831a7f0ea48dc5aa0
Ajay Ravindra Sahu <asahu6@worldbankgroup.org>
2022-10-31 10:48:41 -0400

    api update

--- pages/api/onboarding/getDomain.js
+++ pages/api/onboarding/getDomain.js
@@ -4,21 +4,25 @@ import { dbConnection } from '../../../services/db_connections'
 
 
 async function handler(req, res) {
-    // const session = await getSession({ req })
-    // if (!session) {
-    //     res.status(401).json({ error: 'Unauthenticated user' })
-    // }
-    console.log("Process: ", process.env.DB_URL)
+    if (req.method == 'GET') {
+        // const session = await getSession({ req })
+        // if (!session) {
+        //     res.status(401).json({ error: 'Unauthenticated user' })
+        // }
+        // console.log("Process: ", process.env.DB_URL)
 
-    let query = `select * from SourceSetup.InfoDomain`
-    let domainList = await dbConnection(query)
+        let query = `select * from SourceSetup.InfoDomain`
+        let domainList = await dbConnection(query)
 
-    if (_.isArray(domainList) && domainList.length > 0) {
-        res.status(200).json({ message: 'Success', domainList: domainList })
-    } else {
-        res.status(200).json({ message: 'Domain not found', domainList: [] })
+        if (_.isArray(domainList) && domainList.length > 0) {
+            res.status(200).json({ message: 'Success', domainList: domainList })
+        } else {
+            res.status(200).json({ message: 'Domain not found', domainList: [] })
+        }
     }
-
+    else {
+        res.status(500).json({ message: 'Only GET request allowed' })
+    }
 
 }
 
