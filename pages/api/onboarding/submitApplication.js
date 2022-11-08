8c10ceaaeb58f983a3c2a806c7bed9de7d813ad1
Ajay Ravindra Sahu <asahu6@worldbankgroup.org>
2022-11-08 21:22:08 +0530

    update

--- pages/api/onboarding/submitApplication.js
+++ pages/api/onboarding/submitApplication.js
@@ -23,42 +23,47 @@ async function handler(req, res) {
         }
 
         if (body.domain && body.subDomain && body.applicationName && body.applicationAbbreviatedName && body.applicationDesc && body.statusCode) {
+            try {
+                let connPool = await dbConnection()
+                let result = await connPool.request()
+                    .input('Info_Domain_Code', data.domain)
+                    .input('Info_SubDomain_Code', data.subDomain)
+                    .input('DataSource_App_Code', data.applicationName)
+                    .input('DataSource_App_Abb_Name', data.applicationAbbreviatedName)
+                    .input('DataSource_App_Desc', data.applicationDesc)
+                    .input('StatusCd', data.statusCode)
+                    .input('Primary_Person_Name', data.primaryContactName)
+                    .input('Primary_Email', data.primaryEmailAddress)
+                    .input('Primary_Tel', data.primaryContactNumber)
+                    .input('Secondary_Person_Name', data.secondaryContactName)
+                    .input('Secondary_Email', data.secondaryEmailAddress)
+                    .input('Secondary_Tel', data.secondaryContactNumber)
+                    .input('ITSupport_Email', data.ITSupportEmailAddress)
+                    .input('ITSupport_ContactNumber', '')
+                    .execute(`LoadControl.SP_PopulateOnBoardingApp`);
 
-            let connPool = await dbConnection()
-            let result = await connPool.request()
-                .input('Info_Domain_Code', data.domain)
-                .input('Info_SubDomain_Code', data.subDomain)
-                .input('DataSource_App_Code', data.applicationName)
-                .input('DataSource_App_Abb_Name', data.applicationAbbreviatedName)
-                .input('DataSource_App_Desc', data.applicationDesc)
-                .input('StatusCd', data.statusCode)
-                .input('Primary_Person_Name', data.primaryContactName)
-                .input('Primary_Email', data.primaryEmailAddress)
-                .input('Primary_Tel', data.primaryContactNumber)
-                .input('Secondary_Person_Name', data.secondaryContactName)
-                .input('Secondary_Email', data.secondaryEmailAddress)
-                .input('Secondary_Tel', data.secondaryContactNumber)
-                .input('ITSupport_Email', data.ITSupportEmailAddress)
-                .input('ITSupport_ContactNumber', '')
-                .execute(`LoadControl.SP_PopulateOnBoardingApp`);
+                connPool.close()
+                console.log("End time: ", moment().format('DD-MM-YYYY hh:mm:ss'))
+                let rowsAffected = result.rowsAffected
+                // console.log("rowsAffected: ", rowsAffected)
+                // console.log("result: ", result)
 
-            connPool.close()
-            console.log("End time: ", moment().format('DD-MM-YYYY hh:mm:ss'))
-            let rowsAffected = result.rowsAffected
-            // console.log("rowsAffected: ", rowsAffected)
-            // console.log("result: ", result)
-
-            if (_.isArray(rowsAffected) && rowsAffected.length > 0) {
-                res.status(200).json({ status: 'Success', message: `${rowsAffected[0]} record added` })
-            } else {
-                res.status(200).json({ status: 'Failed', message: 'Failed to add record' })
+                if (_.isArray(rowsAffected) && rowsAffected.length > 0) {
+                    res.status(200).json({ status: 'Success', message: `Application submitted successfully` })
+                } else {
+                    res.status(200).json({ status: 'Failed', message: 'Failed to add record' })
+                }
+            }
+            catch (err) {
+                // console.log("Err: ", err)
+                res.status(500).json({ message: 'Internal Server Error,Please check for duplicate entries' })
             }
         } else {
-            res.status(500).json({ message: 'Invalid data passed' })
+            res.status(400).json({ message: 'Invalid data passed' })
         }
 
     } else {
-        res.status(500).json({ message: 'Only POST request allowed' })
+        res.status(400).json({ message: 'Only POST request allowed' })
     }
 
 }
