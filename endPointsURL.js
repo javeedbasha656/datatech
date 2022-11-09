151515fa7bb2f93334221f525d0eb8a3d1fabbe6
Javeed Basha Hayath Basha <jhayathbasha@worldbankgroup.org>
2022-11-09 08:02:07 -0500

    onboarding application submit api integration and ui changes

--- endPointsURL.js
+++ endPointsURL.js
@@ -1,3 +1,4 @@
 export const TestUserAPI = 'https://jsonplaceholder.typicode.com/users'
 export const DomainAPIURL = '/api/onboarding/getDomain'
 export const SubDomainAPIURL = '/api/onboarding/getSubDomain'
+export const SubmitAppAPIURL = '/api/onboarding/submitApplication'
