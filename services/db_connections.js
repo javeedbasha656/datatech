28d7f4b9a2889edf130d5f14b3cfccba3b596e2b
Ajay Ravindra Sahu <asahu6@worldbankgroup.org>
2022-11-17 17:28:22 +0530

    update

--- services/db_connections.js
+++ services/db_connections.js
@@ -3,7 +3,7 @@ import moment from 'moment'
 
 export async function dbConnection(query) {
     try {
-        console.log("Start time: ", moment().format('DD-MM-YYYY hh:mm:ss'))
+        // console.log("Start time: ", moment().format('DD-MM-YYYY hh:mm:ss'))
         let connPool = await sql.connect(process.env.DB_URL)
 
         return connPool
